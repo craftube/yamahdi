a648e7f30cc07d91ec107d650a5ece173f5f1fae
craftube <130178577+craftube@users.noreply.github.com>
2024-12-10 22:33:19 +0300

    __init__.py

--- JoKeRUB/sql_helper/__init__.py
+++ JoKeRUB/sql_helper/__init__.py
@@ -1,8 +1,4 @@
 import os
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import scoped_session, sessionmaker
-
 # the secret configuration specific things
 from ..Config import Config
 from ..core.logger import logging
